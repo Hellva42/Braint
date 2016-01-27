767df4bc9e0b092d506579b202d40bdecec37524
Torgue41 <>
2016-01-27 18:22:55 +0100

    blah

--- Java/Braint/Agent.java
+++ Java/Braint/Agent.java
@@ -27,16 +27,10 @@ public class Agent {
 
     public void update1(BraintAgentDraw bad) {
 
-        //alternative
-        angle = bad.noise(p.x / bad.getNoiseScale(), p.y / bad.getNoiseScale()) * bad.getNoiseStrength();
 
-        //angle = bad.noise(p.x, p.y);
+        angle = bad.noise(p.x / bad.getNoiseScale(), p.y / bad.getNoiseScale()) * bad.getNoiseStrength();
         p.x += bad.cos(angle) * stepSize;
         p.y += bad.sin(angle) * stepSize;
-
-        //System.out.println("(" + pOld.x + "," + pOld.y + ")" + " -> " + "(" + p.x + "," + p.y + ")" + "  Angle = " + angle);
-        //System.out.println(bad.cos(angle));
-        //System.out.println(bad.noise(p.x / bad.getNoiseScale(), p.y / bad.getNoiseScale()));
 
 
         if(p.x < -10) isOutside = true;
@@ -53,33 +47,9 @@ public class Agent {
 
         }
 
-        /* use for colour mapping to strokes
-        int rgb;
-        int rnd = Math.round(bad.random(1,5));
-        if (rnd == 1) rgb = 0x00640000;
-        else if (rnd == 2) rgb = 0xB2222200;
-        else if (rnd == 3) rgb = 0xFFFF0000;
-        else if (rnd == 4) rgb = 0x0000FF00;
-        else rgb = 0xFF149300;
-        */
 
-
-        /**
-         * Variables:
-         * stroke(rgb colour) see above
-         * stroke(gray colour) value between 1 - 255
-         *
-         *
-         */
-
-        /**
-         * todo : strokeWeigth(strokeWidth*length)
-         */
-//        strokeWeight(strokeWidth*length);
         float strokeWidth = 1;
         bad.strokeWeight(strokeWidth*bad.getStrokeWidthScale());
-        //bad.stroke(rgb);
-        //bad.stroke(125);
         bad.stroke(bad.getRGB());
         bad.line(pOld.x, pOld.y, p.x, p.y);
         pOld.set(p);
--- Java/Braint/BraintAgentDraw.java
+++ Java/Braint/BraintAgentDraw.java
@@ -23,8 +23,9 @@ public class BraintAgentDraw extends PApplet {
     Agent[] agents = new Agent[10000];
     float strokeWidthScale;
     float noiseScale, noiseStrength;
-    int timer;
+    int timerA, timerB, timerC;
     int rgb;
+    float valueToRGB;
 
 
 
@@ -51,52 +52,57 @@ public class BraintAgentDraw extends PApplet {
             agents[i] = new Agent(this);
         }
         strokeWidthScale = 0.3f;
-        noiseStrength = 10f;
+        noiseStrength = 14f;
         noiseScale = 300f;
-        timer = 0;
-        rgb = 0xff2b2b2b;
+        valueToRGB = 1;
+        rgb = decideRGBValue(valueToRGB, 1);
+        timerA = 0;
+        timerB = 0;
+        timerC = 0;
     }
 
     public void draw(){
 
-        timer ++;
+        //remove when added below codeblock
+        timerA++;
+        timerB++;
+        timerC++;
 
-        if (timer > 200) timer = 1;
+        if ( timerA > 20) timerA = 1;
+        if (timerB > 50) timerB = 1;
+        if (timerC > 70) timerC = 1;
 
-        if (timer > 160) {
-            rgb = 0xffbbff88;
-            //rgb = 0xffa81919;
-            noiseStrength = 12;
-            noiseScale = 450;
-        //} else if ( timer > 120 ) {
-          //  rgb = 0xff7b99ae;
-            //noiseStrength = 16;
-        } else if ( timer > 80 ) {
-            rgb = 0xff7c0000;
-            noiseStrength = 7;
-            noiseScale = 200;
-        //} else if (timer > 40) {
-          //  rgb = 0xffbbff88;
-            //noiseStrength = 14;
+        //***variable*** codeblock for setting the variables to emotiv values
+
+        //setVariables();
+
+        //todo replace with function to set valueToRGB to EmotivValue
+        if(valueToRGB < 1.13f && timerA == 20) valueToRGB = valueToRGB + 0.05f;
+        else if (valueToRGB >= 1.13f && timerA == 20)valueToRGB = 0;
+
+        rgb = decideRGBValue(valueToRGB, 2);
+
+        //todo replace with function so set noiseScale to EmotivValue
+        if(noiseScale < 800 && timerB == 50) noiseScale = noiseScale + 50f;
+        else if(noiseScale >= 800 && timerB == 50) noiseScale = 200;
+
+        //todo replace with function to set noiseStrength to EmotivValue
+        if(noiseStrength < 22 && timerB == 70) noiseStrength = noiseStrength + 4;
+        else if(noiseStrength >= 22 && timerB == 70) noiseStrength = 10;
+
+        //todo: add function to set noisestrokeWidthScale to EmotivValue
+        //code to be added here.
+
+        //***variable*** codeblock ending
+
+
+
+
+        //draw everything with the changed values
+        for(int i = 0; i < agents.length; i++) {
+            agents[i].update1(this);
         }
 
-
-
-
-
-//        if (drawMode == 1) {
-            for(int i = 0; i < agents.length; i++) {
-                agents[i].update1(this);
-                /*
-                if (i < 2500)  noiseScale = 1;
-                else if (i >= 2500 && i < 5000) noiseScale = 0.666f;
-                else if (i >= 5000 && i < 7500) noiseScale = 0.333f;
-                else noiseScale = 4;
-                */
-            }
-//        } else {
-//            for(int i =0; i<agentsCount; i++) agents[i].update2();
-//        }
 
 
         /*
@@ -106,9 +112,85 @@ public class BraintAgentDraw extends PApplet {
         */
     }
 
-    public float getStrokeWidthScale() {
-        return strokeWidthScale;
+
+    /**
+     * takes one array with values through osc and returns this array
+     * normalizes every value to values between 0.3 and 1(values that are greater than the global maximum will be between
+     * 1 and 1.5) with a global maximum and minimum.
+     * @return float array with values
+     */
+    //public float[] getEmotivValues(){
+      //  return
+    //}
+
+    /**
+     * set all variable parameters for drawing, depending on choosed array slots
+     * @param emotivValues
+     * @param a Arrayslot for noiseScale value
+     * @param b Arrayslot for noiseStrength value
+     * @param c Arrayslot for strokeWidthScale value
+     * @param d Arrayslot for rgb value
+     */
+
+  public void setVariables(float[] emotivValues , int a, int b, int c, int d) {
+
+      //**Begin NoiseScale**
+
+      //**End NoiseScale**
+
+
+      //**Begin NoiseStrength**
+
+      //**EndNoiseStrength**
+
+
+      //**Begin strokeWidthScale**
+
+      //**End strokeWidthScale**
+
+
+
+      //**Begin rgb value**
+
+      //**End rgb value**
+
+
+  }
+
+    /**
+     * Chooses the colour in which the next strokes should be drawn depending on given value and colour mode,
+     * @param value value for which the colour should be picked
+     * @param mode colour mode: 2 = yellow/red , 1 = green
+     * @return hex value for stroke function
+     * todo: change mode 2
+     */
+    public int decideRGBValue(float value, int mode){
+
+        if(mode == 1) {
+            if( value < 0.1666f) return 0xffccece6;
+            if( value < 0.3333f) return 0xff99d8c9;
+            if( value < 0.5f) return 0xff66c2a4;
+            if( value < 0.6666f) return 0xff41ae76;
+            if( value < 0.8333f) return 0xff238b45;
+            if( value < 1f) return 0xff006d2c;
+            return 0xff00441b;
+
+        } else  if(mode == 2){
+            if( value < 0.1666f) return 0xfffdd49e;
+            if( value < 0.3333f) return 0xfffdbb84;
+            if( value < 0.5f) return 0xfffc8d59;
+            if( value < 0.6666f) return 0xffef6548;
+            if( value < 0.8333f) return 0xffd7301f;
+            if( value < 1f) return 0xffb30000;
+            return 0xff7f0000;
+
+        }
+
+        return 0;
     }
+
+
+    public float getStrokeWidthScale() { return strokeWidthScale; }
     public float getNoiseScale() { return noiseScale; }
     public float getNoiseStrength() { return noiseStrength; }
     public int getRGB() { return rgb; }
